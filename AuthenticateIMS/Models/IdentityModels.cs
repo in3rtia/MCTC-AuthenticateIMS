5464b5fd03529a926571044471028e2936fc26db
ricchard <ricch.mwaba@gmail.com>
2017-11-20 10:44:23 +0200

    Initial commit

---
+++ AuthenticateIMS/Models/IdentityModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AuthenticateIMS.Extensions;
+
+namespace AuthenticateIMS.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(60)]
+        [Index(IsUnique = true)]
+        [Display(Name = "Mine Number")]
+        public string MineNumber { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            userIdentity.AddClaim(new Claim("MineNumber", this.MineNumber.ToString()));
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
